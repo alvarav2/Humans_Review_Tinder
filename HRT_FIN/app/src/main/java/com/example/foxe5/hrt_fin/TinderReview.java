9d730498655653a9f9f2f38f211e5f99f78ed653
Elliot Rose Fox <foxe5@LabImage2016-17>
2019-02-16 23:28:42 -0800

    THE BUTTON WORKS

---
+++ HRT_FIN/app/src/main/java/com/example/foxe5/hrt_fin/TinderReview.java
@@ -0,0 +1,23 @@
+package com.example.foxe5.hrt_fin;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * Created by foxe5 on 2/16/19.
+ */
+
+public class TinderReview extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tinder_profile);
+    }
+
+}
